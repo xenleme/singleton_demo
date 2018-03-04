16c58d60dfa927807464ccc43ee814d9db55a0ee
xenleme <xenleme@gmail.com>
2018-03-04 12:08:58 +0300

    update logger

--- app.rb
+++ app.rb
@@ -1,7 +1,18 @@
 class Logger
+  def initialize
+    @f = File.open('log.txt', 'a')
+  end
+
   def self.say_something
     puts 'Something...'
+  end
+
+  def log_something(what)
+    @f.puts what
   end
 end
 
 Logger.say_something
+
+logger = Logger.new
+logger.log_something('hey!')
